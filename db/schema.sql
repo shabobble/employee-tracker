55f2846348108a0a97d48217bc493448495e31ef
shabobble <pksullivan@yahoo.com>
2021-05-24 14:19:38 -0400

    added schema.sql file

---
+++ db/schema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS employeeMgrDB;
+
+CREATE DATABASE employeeMgrDB;
+
+USE employeeMgrDB;
+
+CREATE TABLE employees (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INTEGER(10),
+    manager_id INTEGER(10),
+);
+
+CREATE TABLE roles (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL(7, 2),
+    department_id INTEGER NOT NULL
+);
+
+CREATE TABLE departments (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY
+    dept_name VARCHAR(30),
+);
+
