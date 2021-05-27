cd39ecc4d26efbda74c1c7b30cf9e263ad33a1dd
shabobble <pksullivan@yahoo.com>
2021-05-26 20:22:30 -0400

    added viewByMng.js

---
+++ controllers/viewByMng.js
@@ -0,0 +1,22 @@
+const inquirer = require('inquirer');
+const { getAllManagers } = require('./getAll');
+
+const viewByMng = () => {
+    return new Promise ((resolve, reject) => {
+        getAllManagers()
+        .then((allManagers) => {
+            inquirer.prompt([
+                {
+                    type: 'list',
+                    name: 'managerId',
+                    message: 'Which manager would you like to view?',
+                    choices: allManagers
+                }
+            ])
+        })
+        .then((answers) => resolve(answers))
+        .catch((err) => reject(err))
+    });
+}
+
+module.exports = viewByMng;
