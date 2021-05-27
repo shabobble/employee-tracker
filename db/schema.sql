2e08a5d4ab69e81d71b49bfe459597c808a0dd98
shabobble <pksullivan@yahoo.com>
2021-05-26 20:23:51 -0400

    updated schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -5,22 +5,24 @@ CREATE DATABASE employeeMgrDB;
 USE employeeMgrDB;
 
 CREATE TABLE employees (
-    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INTEGER NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INTEGER(10),
-    manager_id INTEGER(10)
+    manager_id INTEGER(10),
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE roles (
-    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INTEGER NOT NULL AUTO_INCREMENT,
     title VARCHAR(30),
     salary DECIMAL(7, 2),
-    department_id INTEGER NOT NULL
+    department_id INTEGER NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE departments (
-    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY
-    dept_name VARCHAR(30)
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    dept_name VARCHAR(30),
+    PRIMARY KEY (id)
 );
-
