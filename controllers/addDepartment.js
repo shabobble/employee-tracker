44aea379ddcd9282bd33a60417cc09e2e49c1f72
shabobble <pksullivan@yahoo.com>
2021-05-26 20:15:18 -0400

    added addDepartment.js

---
+++ controllers/addDepartment.js
@@ -0,0 +1,28 @@
+const inquirer = require('inquirer');
+const connection = require('../config/db');
+
+const addDept = (askTask) => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'What is the name of the department?',
+            name: 'deptName',
+        }
+    ])
+    .then( (answers) => {
+        console.log(answers)
+        connection.query
+            'INSERT INTO departments SET ?',
+            {
+                dept_name: answers.deptName,
+            },
+            function(err) {
+                console.log(err)
+                if (err) throw err;
+                console.log('Successfully added new department');
+                askTask();
+            }
+    })
+}
+
+module.exports = addDept;
