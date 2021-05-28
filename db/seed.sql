fdc43b58482688ea02290c4b4a48ca39aa2a8f49
shabobble <pksullivan@yahoo.com>
2021-05-27 21:31:38 -0400

    added seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -1,10 +1,11 @@
+
 -- Employees Starter Table
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Michael', 'Scott', 1, 1);
 INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Dwight', 'Schrute', 2, 1);
 INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Jim', 'Halpert', 3, 1);
 INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Toby', 'Flenderson', 4, 3);
-INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Pam', 'Beasely', 5, 2;
+INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Pam', 'Beasely', 5, 2);
 INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Darryl', 'Philbin', 6, 5);
 INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Andy', 'Bernard', 7, 2);
 INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ('Angela', 'Martin', 8, 4);
@@ -27,8 +28,8 @@ INSERT INTO roles (title, salary, department_id) VALUES ('Accountant', 55000, 4)
 
 -- Department Starter Table
 
-INSERT INTO departments (dept_name) VALUES ('Management')
-INSERT INTO departments (dept_name) VALUES ('Sales & Service')
-INSERT INTO departments (dept_name) VALUES ('Human Resources')
-INSERT INTO departments (dept_name) VALUES ('Accounting')
-INSERT INTO departments (dept_name) VALUES ('Warehouse')
+INSERT INTO departments (dept_name) VALUES ('Management');
+INSERT INTO departments (dept_name) VALUES ('Sales & Service');
+INSERT INTO departments (dept_name) VALUES ('Human Resources');
+INSERT INTO departments (dept_name) VALUES ('Accounting');
+INSERT INTO departments (dept_name) VALUES ('Warehouse');
